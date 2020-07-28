0ffc2c0ef2014e6fdd6e1afb4232e03905c58a4d
lily.goli <leilygoli100@yahoo.com>
2020-07-28 11:50:33 +0430

    Added HTML files. Delete client code. Add safe exit from clients.

--- Server.py
+++ Server.py
@@ -2,30 +2,37 @@ import socket
 import threading
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 8080        # Port to listen on (non-privileged ports are > 1023)
+PORT = 8080  # Port to listen on (non-privileged ports are > 1023)
 
 
 class Server:
-
     def client_handler(self, clnt, addr):
-        with clnt:
-            print('Connected by', addr)
-            print(type(clnt))
-            while True:
-                data = clnt.recv(1024)
-                if not data:
-                    break
-                clnt.sendall(data)
+        try:
+            with clnt:
+                print('Connected by', addr)
+                print(type(clnt))
+                while True:
+                    data = clnt.recv(1024)
+                    if not data:
+                        break
+                    clnt.sendall(data)
+        except ConnectionResetError:
+            clnt.close()
 
     def listen(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((HOST, PORT))
             s.listen()
-            while (True):
+            while True:
                 clnt, addr = s.accept()
-                handler = threading.Thread(target=self.client_handler, args=(clnt, addr, ),daemon=False)
+                handler = threading.Thread(target=self.client_handler, args=(clnt, addr,), daemon=False)
                 handler.start()
 
 
-server = Server()
-server.listen()
+def main():
+    server = Server()
+    server.listen()
+
+
+if __name__ == "__main__":
+    main()
