53cc4d7af7bd2113cb4b28f4bb29d944ae9cc125
lily.goli <leilygoli100@yahoo.com>
2020-07-28 12:59:54 +0430

    Add response to server. (only: 200 ok)

--- Server.py
+++ Server.py
@@ -1,5 +1,8 @@
 import socket
 import threading
+from wsgiref.handlers import format_date_time
+from datetime import datetime
+from time import mktime
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 8080  # Port to listen on (non-privileged ports are > 1023)
@@ -7,9 +10,27 @@ PORT = 8080  # Port to listen on (non-privileged ports are > 1023)
 
 class Server:
 
-    def response_maker(self, data_dict, request_details):
-        pass
+    def get_time(self):
+        now = datetime.now()
+        stamp = mktime(now.timetuple())
+        return  format_date_time(stamp)
 
+    def response_maker(self, data_dict, request_details):
+        code = '200 OK\r\n'
+        response_str = request_details[2] + " "
+        response_str += code
+        response_str += 'Connection: '+ data_dict['Connection'] + '\r\n'
+        file = open("Pages/main.html", "r")
+        content = file.read()
+        encoded_content = content.encode()
+        response_str += 'Connection-Length: ' + str(len(encoded_content)) + '\r\n'
+        response_str += 'Content-Type: text/html' + '\r\n'
+        response_str += 'Date: ' + str(self.get_time()) + '\r\n'
+        response_str += '\r\n'
+        response_str += content
+        # response_str.encode()
+        response_str = bytes(response_str, 'utf-8')
+        return response_str
 
     def client_handler(self, clnt, addr):
         try:
@@ -22,17 +43,17 @@ class Server:
                         data_split = data.split("\r\n")
                         data_request = data_split[0]
                         request_details = data_request.split(" ")
-                        print(request_details)
+                        # print(request_details)
                         data_dict = dict()
                         for i in range(1, len(data_split)):
                             if ":" in data_split[i]:
                                 elements = data_split[i].split(":", 2)
                                 data_dict[elements[0].strip()] = elements[1].strip()
-                        self.response_maker(data_dict, request_details)
+                        response = self.response_maker(data_dict, request_details)
                         #print(data_dict)
                     if not data:
                         break
-                    #clnt.sendall(data)
+                    clnt.sendall(response)
         except ConnectionResetError:
             clnt.close()
 
