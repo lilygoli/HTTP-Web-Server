7486429c079320545ac5b54aea475bc73380710d
mahsa <mahsadalir91@gmail.com>
2020-07-28 11:31:20 +0430

    start multi-thread programming

--- Client.py
+++ Client.py
@@ -7,5 +7,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b'Hello, world')
     data = s.recv(1024)
-
+    while (True):
+        continue
 print('Received', repr(data))
--- Server.py
+++ Server.py
@@ -1,16 +1,31 @@
 import socket
+import threading
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 8080        # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    clnt, addr = s.accept()
-    with clnt:
-        print('Connected by', addr)
-        while True:
-            data = clnt.recv(1024)
-            if not data:
-                break
-            clnt.sendall(data)
+
+class Server:
+
+    def client_handler(self, clnt, addr):
+        with clnt:
+            print('Connected by', addr)
+            print(type(clnt))
+            while True:
+                data = clnt.recv(1024)
+                if not data:
+                    break
+                clnt.sendall(data)
+
+    def listen(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen()
+            while (True):
+                clnt, addr = s.accept()
+                handler = threading.Thread(target=self.client_handler, args=(clnt, addr, ),daemon=False)
+                handler.start()
+
+
+server = Server()
+server.listen()
