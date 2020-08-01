9d67c839a75bcb645ac97656ad3d0820400e43c4
lily.goli <leilygoli100@yahoo.com>
2020-08-01 12:24:08 +0430

    Proxy logs added.

--- Proxy.py
+++ Proxy.py
@@ -1,5 +1,8 @@
 import socket
 import threading
+from datetime import datetime
+from time import mktime
+from wsgiref.handlers import format_date_time
 
 client_port = 8090
 command_port = 8091
@@ -7,6 +10,11 @@ HOST = '127.0.0.1'
 
 
 class Proxy:
+
+    def get_time(self):
+        now = datetime.now()
+        stamp = mktime(now.timetuple())
+        return format_date_time(stamp)
 
     def client_handler(self, clnt, addr):
         try:
@@ -17,31 +25,22 @@ class Proxy:
                     data_split = data.split("\r\n")
                     data_request = data_split[0]
                     request_type = data_request.split(" ")[0]
+                    port, web_server = self.parse_request(data_request)
+                    print("Request:", "[" + self.get_time() + "]", "[" + addr[0] + ":" + str(addr[1]) + "]",
+                          "[" + web_server + ":" + str(port) + "]", '"' + data_request + '"')
                     if request_type == "GET":
-                        print(data_request)
-                        url = data_request.split(" ")[1]
-                        http_pos = url.find("://")
-                        if http_pos == -1:
-                            temp = url
-                        else:
-                            temp = url[(http_pos + 3):]
-                        port_pos = temp.find(":")
-                        web_server_pos = temp.find("/")
-                        if web_server_pos == -1:
-                            web_server_pos = len(temp)
-                        if port_pos == -1 or web_server_pos < port_pos:
-                            port = 80
-                            web_server = temp[:web_server_pos]
-                        else:
-                            port = int((temp[(port_pos + 1):])[:web_server_pos - port_pos - 1])
-                            web_server = temp[:port_pos]
                         data = bytes(data, 'utf-8')
-                        print(web_server, port)
                         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                             s.connect((web_server, port))
                             s.sendall(data)
                             while True:
                                 answer = s.recv(1024)
+                                a = answer.decode('utf-8', errors='replace')
+                                if a.startswith("HTTP"):
+                                    http_status = a.split("\n")[0]
+                                    print("Response:", "[" + self.get_time() + "]",
+                                          "[" + addr[0] + ":" + str(addr[1]) + "]",
+                                          "[" + web_server + ":" + str(port) + "]", '"'+http_status[:-1]+'" for '+'"' + data_request + '"')
                                 if len(answer) > 0:
                                     clnt.sendall(answer)
                                 else:
@@ -50,6 +49,25 @@ class Proxy:
                     clnt.close()
         except ConnectionResetError:
             clnt.close()
+
+    def parse_request(self, data_request):
+        url = data_request.split(" ")[1]
+        http_pos = url.find("://")
+        if http_pos == -1:
+            temp = url
+        else:
+            temp = url[(http_pos + 3):]
+        port_pos = temp.find(":")
+        web_server_pos = temp.find("/")
+        if web_server_pos == -1:
+            web_server_pos = len(temp)
+        if port_pos == -1 or web_server_pos < port_pos:
+            port = 80
+            web_server = temp[:web_server_pos]
+        else:
+            port = int((temp[(port_pos + 1):])[:web_server_pos - port_pos - 1])
+            web_server = temp[:port_pos]
+        return port, web_server
 
     def client_listen(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
